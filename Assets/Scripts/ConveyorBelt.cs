e33e29c23cafaa04e84fccecff2dc290e0596f42
JQ <chulini@gmail.com>
2020-04-18 19:54:39 +0200

    conveyor belt speed changes shader

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -10,10 +10,15 @@ public class ConveyorBelt : MonoBehaviour
     [SerializeField] private FloatVariable conveyorBeltSpeed;
 #pragma warning restore 0649
     private Rigidbody _rigidbody;
+    private Renderer _renderer;
+    private MaterialPropertyBlock _materialPropertyBlock;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+        _materialPropertyBlock = new MaterialPropertyBlock();
+        
     }
 
     private void FixedUpdate()
@@ -21,5 +26,9 @@ public class ConveyorBelt : MonoBehaviour
         Vector3 pos = _rigidbody.position;
         _rigidbody.position += Vector3.back * conveyorBeltSpeed.Value * Time.fixedDeltaTime;
         _rigidbody.MovePosition(pos);
+        
+        _renderer.GetPropertyBlock(_materialPropertyBlock);
+        _materialPropertyBlock.SetFloat("Vector1_B5B16460", conveyorBeltSpeed.Value);
+        _renderer.SetPropertyBlock(_materialPropertyBlock);
     }
 }
