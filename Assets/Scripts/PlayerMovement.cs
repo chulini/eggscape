e6a631f704608a373b1b7e5299ae17b5cf8bfdd5
JQ <chulini@gmail.com>
2020-04-18 15:23:35 +0200

    rewired

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,107 +1,43 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using ScriptableObjectArchitecture;
 using UnityEngine;
 
-namespace Player
+public class PlayerMovement : MonoBehaviour
 {
-    /// <summary>
-    /// Listen to the input SO and moves/jump the player
-    /// </summary>
-    public class PlayerMovement : MonoBehaviour
-    {
 #pragma warning disable 0649
-        [Header("Scriptable Objects (Reading)")]
-        [SerializeField] private GameObjectReference _cameraReference;
-        [SerializeField] private FloatReference _xAxis;
-        [SerializeField] private FloatReference _yAxis;
-        [SerializeField] private FloatReference _rotationSpeed;
-        [SerializeField] private FloatReference _walkingSpeed;
-        [SerializeField] private FloatReference _runningSpeed;
-        [SerializeField] private BoolReference _isRunning;
-        [SerializeField] private BoolReference _isGrounded;
-        [SerializeField] private BoolReference _jumpButtonPressed;
-        [SerializeField] private FloatVariable _jumpForce;
-        [SerializeField] private FloatVariable _jumpForceForwardBoost;
-        
+    [SerializeField] private Vector2Reference torqueMaxPower;
+    [Header("Scriptable Objects (Reading)")]
+    [SerializeField] private FloatVariable _xAxis;
+    [SerializeField] private FloatVariable _yAxis;
+    [SerializeField] private FloatVariable axisThreshold;
+    [SerializeField] private GameObjectVariable cameraGameObject;
 #pragma warning restore 0649
-        private Transform _cameraTransform;
-        private Rigidbody _rigidbody;
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    private Transform _cameraTransform;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = transform;
+    }
+
+    private void Start()
+    {
+        _cameraTransform = cameraGameObject.Value.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log($"{_xAxis} {_yAxis}");
+        // Vector2 torquePower = Vector2.zero;
+        // // Forward/Backwards torque
+        // if (Mathf.Abs(_yAxis.Value) > axisThreshold.Value)
+        // {
+        //     torquePower = torquePower.WithX(torqueMaxPower.Value.y*_yAxis.Value);
+        // }
         
-        private Vector3 _lastValidLookingDirection;
-        private bool _forceNotGrounded;
-        
-
-        private void OnEnable()
-        {
-            _jumpButtonPressed.AddListener(JumpButtonPressedChanged);
-        }
-        private void OnDisable()
-        {
-            _jumpButtonPressed.RemoveListener(JumpButtonPressedChanged);
-        }
-        private void JumpButtonPressedChanged()
-        {
-            if (_jumpButtonPressed.Value && _isGrounded.Value)
-            {
-                Vector3 currentVel = _rigidbody.velocity.WithY(0);
-                _rigidbody.AddForce(
-                    currentVel.normalized * (_jumpForce.Value * _jumpForceForwardBoost.Value) + 
-                    Vector3.up * (_jumpForce.Value)
-                    , ForceMode.Impulse);
-                
-                // Asume is not gounded for a small time so vectical force
-                // can act and then start checking if its really grounded
-                _forceNotGrounded = true;
-                Invoke("StopForcingNotGrounded",.2f);
-            }
-        }
-
-
-        private void Start()
-        {
-            _cameraTransform = _cameraReference.Value.GetComponent<Transform>();
-            _rigidbody = GetComponent<Rigidbody>();
-            _lastValidLookingDirection = new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z).normalized;
-        }
-
-        
-        void StopForcingNotGrounded()
-        {
-            _forceNotGrounded = false;
-        }
-        void FixedUpdate()
-        {
-
-            if (_forceNotGrounded || !_isGrounded.Value)
-            {
-                // Debug.Log($"is not grounded");
-                // If its jumping don't respond to rotation
-                 _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, Quaternion.LookRotation(_lastValidLookingDirection.normalized), Time.fixedDeltaTime * _rotationSpeed.Value);
-                return;
-            }
-
-            Vector3 forwardDirection =
-                new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z).normalized;
-            Vector3 rightDirection = (Quaternion.Euler(0f, 90f, 0f) * forwardDirection).normalized;
-
-            Vector3 currentLookingDirection = Vector3.zero;
-            currentLookingDirection += forwardDirection * _yAxis.Value;
-            currentLookingDirection += rightDirection * _xAxis.Value;
-            currentLookingDirection.y = 0.0f;
-
-            float currentSpeed = _isRunning.Value ? _runningSpeed.Value : _walkingSpeed.Value;
-            if (currentLookingDirection.magnitude > 0.01f)
-                _lastValidLookingDirection = (currentLookingDirection.normalized * currentSpeed).WithY(0);
-
-            Quaternion targetRotation = Quaternion.LookRotation(_lastValidLookingDirection.normalized);
-
-            _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation,
-                Time.fixedDeltaTime * _rotationSpeed.Value);
-            _rigidbody.velocity =
-                (currentLookingDirection.magnitude > 0.01f
-                    ? _lastValidLookingDirection * Time.fixedDeltaTime
-                    : Vector3.zero).WithY(_rigidbody.velocity.y);
-
-        }
+        // _rigidbody.AddTorque();
     }
 }
