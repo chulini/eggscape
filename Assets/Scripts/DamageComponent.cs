20924e6b3e428c266194cc3d80779d405f5e8c94
Pieter Fourie <pieteruys.fourie@gmail.com>
2020-04-18 22:50:36 +0200

    DOTs

--- Assets/Scripts/DamageComponent.cs
+++ Assets/Scripts/DamageComponent.cs
@@ -1,23 +1,47 @@
-﻿using ScriptableObjectArchitecture;
+﻿using System;
+using System.Collections;
+using ScriptableObjectArchitecture;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DamageComponent : MonoBehaviour
 {
-    [SerializeField] private Vector2 _damageRange = new Vector2(1, 3);
+    [SerializeField] private Vector2 _damageRange = new Vector2(1, 10);
+    [SerializeField] private float _damageInterval = 1f;
     
     [SerializeField] private FloatReference _eggHealth;
     [SerializeField] private GameObjectReference _player;
+
+    private Coroutine _damageCoroutine;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == _player.Value)
         {
-            _eggHealth.Value -= CalculateDamage();
+            _damageCoroutine = StartCoroutine(DamagePlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player.Value)
+        {
+            StopCoroutine(_damageCoroutine);
         }
     }
 
     private float CalculateDamage()
     {
         return Random.Range(_damageRange.x, _damageRange.y);
+    }
+
+    private IEnumerator DamagePlayer()
+    {
+        while (true)
+        {
+            _eggHealth.Value -= CalculateDamage();
+            
+            yield return new WaitForSeconds(_damageInterval);
+        }
     }
 }
