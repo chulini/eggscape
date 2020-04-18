559627caeaef41f287b2c230eb8ed81c6e8e43d1
JQ <chulini@gmail.com>
2020-04-18 14:40:36 +0200

    third person camera

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private GameObjectVariable playerGameObject;
+#pragma warning restore 0649
+    private void OnEnable()
+    {
+        playerGameObject.Value = gameObject;
+    }
+
+    private void OnDisable()
+    {
+        playerGameObject.Value = null;
+    }
+}
