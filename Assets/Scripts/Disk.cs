877be69d8a6822eb962181e3dffbcb3ed1770c49
JQ <chulini@gmail.com>
2020-04-18 20:20:17 +0200

    skybox, work in progress disk

---
+++ Assets/Scripts/Disk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class Disk : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private FloatReference diskSpeed;
+#pragma warning restore 0649
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.rotation = Quaternion.Euler(0,_rigidbody.rotation.eulerAngles.y + diskSpeed.Value*Time.fixedDeltaTime,0);
+        // _transform.localRotation = Quaternion.Euler(0,_transform.localRotation.eulerAngles.y + diskSpeed.Value*Time.fixedDeltaTime,0);
+    }
+}
