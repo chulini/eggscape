40d27fbe6a3b694110bc1bc66a85cc3f01f7cbcd
JQ <chulini@gmail.com>
2020-04-19 23:06:10 +0200

    one prefab to rule them all

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Rewired;
+using ScriptableObjectArchitecture;
+using Cursor = UnityEngine.Cursor;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private BoolGameEvent _onPauseEvent;
+    [SerializeField] private GameObject startMenuContainer;
+    [SerializeField] private GameObject pauseMenuContainer;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _onPauseEvent.Raise(true);
+        startMenuContainer.SetActive(true);
+        pauseMenuContainer.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _onPauseEvent.AddListener(PauseEventHandler);
+        gameObject.transform.SetAsLastSibling();
+    }
+
+    private void OnDisable()
+    {
+        _onPauseEvent.RemoveListener(PauseEventHandler);
+    }
+
+    private void PauseEventHandler(bool paused)
+    {
+        Cursor.visible = paused;
+        Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+        startMenuContainer.SetActive(false);
+        pauseMenuContainer.SetActive(paused);
+    }
+
+    public void PlayClicked()
+    {
+        _onPauseEvent.Raise(false);
+    }
+    public void ResumeClicked()
+    {
+        _onPauseEvent.Raise(false);
+    }
+
+
+    public void QuitClicked()
+    {
+        Application.Quit();
+    }
+
+}
