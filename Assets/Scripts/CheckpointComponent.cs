68fe898707c78485adaca09b82c88e184f2a5921
Pieter Fourie <pieteruys.fourie@gmail.com>
2020-04-19 22:23:13 +0200

    checkpint

--- Assets/Scripts/CheckpointComponent.cs
+++ Assets/Scripts/CheckpointComponent.cs
@@ -5,7 +5,6 @@ public class CheckpointComponent : MonoBehaviour
 {
     [SerializeField] private GameObjectReference _player;
     [SerializeField] private CheckpointComponentVariable _activeCheckpoint;
-    [SerializeField] private CheckpointVisualComponent _checkpointVisual;
     [SerializeField] private FloatGameEvent _onPlayerHealed;
     
     private void OnTriggerEnter(Collider other)
@@ -15,23 +14,7 @@ public class CheckpointComponent : MonoBehaviour
             return;
         }
 
-        ActivationHandler();
         HealHandler();
-    }
-
-    private void ActivationHandler()
-    {
-        if (null != _activeCheckpoint.Value && null != _activeCheckpoint.Value._checkpointVisual)
-        {
-            _activeCheckpoint.Value._checkpointVisual.DeactivateCheckpoint();
-        }
-
-        if (null != _checkpointVisual)
-        {
-            _checkpointVisual.ActivateCheckpoint();
-        }
-        
-        _activeCheckpoint.Value = this;
     }
 
     private void HealHandler()
