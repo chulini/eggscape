7e0798a8697600ba5b754ce32129291d4e281031
Pieter Fourie <pieteruys.fourie@gmail.com>
2020-04-19 22:02:37 +0200

    Heal on checkpoint

--- Assets/Scripts/CheckpointComponent.cs
+++ Assets/Scripts/CheckpointComponent.cs
@@ -6,7 +6,8 @@ public class CheckpointComponent : MonoBehaviour
     [SerializeField] private GameObjectReference _player;
     [SerializeField] private CheckpointComponentVariable _activeCheckpoint;
     [SerializeField] private CheckpointVisualComponent _checkpointVisual;
-
+    [SerializeField] private FloatGameEvent _onPlayerHealed;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject != _player.Value)
@@ -14,6 +15,12 @@ public class CheckpointComponent : MonoBehaviour
             return;
         }
 
+        ActivationHandler();
+        HealHandler();
+    }
+
+    private void ActivationHandler()
+    {
         if (null != _activeCheckpoint.Value && null != _activeCheckpoint.Value._checkpointVisual)
         {
             _activeCheckpoint.Value._checkpointVisual.DeactivateCheckpoint();
@@ -25,5 +32,10 @@ public class CheckpointComponent : MonoBehaviour
         }
         
         _activeCheckpoint.Value = this;
+    }
+
+    private void HealHandler()
+    {
+        _onPlayerHealed.Raise(100f);
     }
 }
