84984ddc2cbb741d9ed437885f554c09ec6cd6f6
Pieter Fourie <pieteruys.fourie@gmail.com>
2020-04-19 19:55:50 +0200

    Respawning, fading, pausing etc.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
-﻿using ScriptableObjectArchitecture;
+﻿using System.Collections;
+using ScriptableObjectArchitecture;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,24 +10,148 @@ public class GameManager : MonoBehaviour
     [SerializeField] private FloatReference _eggHealth;
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private CheckpointComponent _levelStartPoint;
+    [SerializeField] private GameEvent _playerDiedEvent;
+    [SerializeField] private GameEvent _onRespawnPlayer;
+    [SerializeField] private BoolGameEvent _onPausedEvent;
+    [SerializeField] private FloatGameEvent _onScreenFade;
+    [SerializeField] private Canvas _screenFade;
+    [SerializeField] private float _fadeChangeStep = 0.05f;
+    [SerializeField] private float _fadeChangeInterval = 0.05f;
+
+    private Coroutine _fadeCoroutine;
+    private Image _fadeImage;
+    private float _currentTimeScale;
 
     private void Awake()
     {
         SetSpawnPoint();
         _playerReference.Value = Instantiate(_playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        _eggHealth.Value = 0;
+        _fadeImage = _screenFade.GetComponentInChildren<Image>();
+    }
+
+    private void Start()
+    {
+        _onRespawnPlayer.Raise();
+        _screenFade.gameObject.SetActive(true);
+        _onScreenFade.Raise(0f);
     }
 
     private void OnDestroy()
     {
         _activeCheckpoint.Value = _levelStartPoint;
     }
+    
+    private void OnEnable()
+    {
+        _eggHealth.AddListener(OnHealthChange);
+        _onScreenFade.AddListener(OnFade);
+        _onPausedEvent.AddListener(OnPause);
+    }
+
+    private void OnDisable() 
+    {
+        _eggHealth.RemoveListener(OnHealthChange);
+        _onScreenFade.RemoveListener(OnFade);
+        _onPausedEvent.RemoveListener(OnPause);
+    }
 
     private void SetSpawnPoint()
     {
-        if (null == _activeCheckpoint.Value)
+        _activeCheckpoint.Value = _levelStartPoint;
+    }
+
+    private void OnHealthChange()
+    {
+        if (_eggHealth.Value > float.Epsilon)
         {
-            _activeCheckpoint.Value = _levelStartPoint;
+            return;
         }
+        
+        _playerDiedEvent.Raise();
+    }
+
+    private void OnFade(float targetOpacity)
+    {
+        if (null != _fadeCoroutine)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+
+        _fadeCoroutine = StartCoroutine(FadeCoroutine(targetOpacity));
+    }
+
+    private IEnumerator FadeCoroutine(float targetOpacity)
+    {
+        var fadeColor = _fadeImage.color; 
+        var currentOpacity = fadeColor.a;
+
+        while (NotAtOpacity(currentOpacity, targetOpacity))
+        {
+            if (currentOpacity < targetOpacity)
+            {
+                SetFadeAlpha(_fadeImage.color.a + _fadeChangeStep);
+
+                if (_fadeImage.color.a > targetOpacity)
+                {
+                    SetFadeAlpha(targetOpacity);
+                }
+                
+                yield return new WaitForSecondsRealtime(_fadeChangeInterval);
+            }
+            else if (currentOpacity > targetOpacity)
+            {
+                SetFadeAlpha(_fadeImage.color.a - _fadeChangeStep);
+
+                if (_fadeImage.color.a < targetOpacity)
+                {
+                    SetFadeAlpha(targetOpacity);
+                }
+
+                yield return new WaitForSecondsRealtime(_fadeChangeInterval);
+            }
+        }
+    }
+    
+    private void SetFadeAlpha(float alpha)
+    {
+        var fadeColor = _fadeImage.color; 
+        
+        _fadeImage.color = new Color(fadeColor.r, fadeColor.g, fadeColor.b, alpha);
+    }
+
+    private static bool NotAtOpacity(float opacity, float targetOpacity)
+    {
+        return opacity < targetOpacity || opacity > targetOpacity;
+    }
+
+    private void OnPause(bool isPaused)
+    {
+        OnPauseFadeHandler(isPaused);
+        OnPauseTimeScaleHandler(isPaused);
+    }
+
+    private void OnPauseFadeHandler(bool isPaused)
+    {
+        if (isPaused)
+        {
+            _onScreenFade.Raise(0.75f);
+
+            return;
+        }
+        
+        _onScreenFade.Raise(0f);
+    }
+
+    private void OnPauseTimeScaleHandler(bool isPaused)
+    {
+        if (isPaused)
+        {
+            _currentTimeScale = Time.timeScale;
+            Time.timeScale = 0f;
+
+            return;
+        }
+
+        Time.timeScale = _currentTimeScale;
     }
 }
--- Assets/Scripts/PlayerSpawnManager.cs
+++ Assets/Scripts/PlayerSpawnManager.cs
@@ -1,34 +1,81 @@
+using System.Collections;
 using ScriptableObjectArchitecture;
 using UnityEngine;
 
 public class PlayerSpawnManager : MonoBehaviour
 {
-    [SerializeField] private FloatReference _eggHealth;
     [SerializeField] private CheckpointComponentVariable _activeCheckpoint;
     [SerializeField] private GameObjectReference _playerReference;
-    
+    [SerializeField] private GameEvent _playerDiedEvent;
+    [SerializeField] private FloatVariable _eggHealth;
+    [SerializeField] private GameEvent _playerSpawnedEvent;
+    [SerializeField] private GameEvent _onRespawnPlayer;
+    [SerializeField] private FloatGameEvent _onScreenFade;
+    [SerializeField] private float _respawnTime = 1f;
+
+    private Rigidbody _playerRigidBody;
+    private Coroutine _dieCoroutine;
+    private Coroutine _spawnCoroutine;
+
+    private void Start()
+    {
+        _playerRigidBody = _playerReference.Value.GetComponent<Rigidbody>();
+    }
+
     private void OnEnable()
     {
-        _eggHealth.AddListener(CheckHealth);
+        _playerDiedEvent.AddListener(OnPlayerDied);
+        _onRespawnPlayer.AddListener(RespawnPlayer);
     }
 
-    private void OnDisable() {
-        _eggHealth.RemoveListener(CheckHealth);
-    }
-
-    private void CheckHealth()
+    private void OnDisable()
     {
-        if (_eggHealth.Value > float.Epsilon)
+        _playerDiedEvent.RemoveListener(OnPlayerDied);
+        _onRespawnPlayer.RemoveListener(RespawnPlayer);
+    }
+
+    private void OnPlayerDied()
+    {
+        Time.timeScale = 0.25f;
+        _onScreenFade.Raise(1f);
+
+        if (null != _dieCoroutine)
         {
-            return;
+            StopCoroutine(_dieCoroutine);
         }
 
-        RespawnPlayer();
+        _dieCoroutine = StartCoroutine(DieCoroutine());
     }
 
     private void RespawnPlayer()
     {
         _eggHealth.Value = 100;
+        _playerRigidBody.velocity = Vector3.zero;
+        _playerRigidBody.angularVelocity = Vector3.zero;
         _playerReference.Value.transform.position = _activeCheckpoint.Value.transform.position;
+        _playerReference.Value.transform.rotation = Quaternion.identity;
+        
+        if (null != _spawnCoroutine)
+        {
+            StopCoroutine(_spawnCoroutine);
+        }
+
+        _spawnCoroutine = StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator DieCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(_respawnTime);
+        
+        _onRespawnPlayer.Raise();
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(_respawnTime);
+
+        Time.timeScale = 1f;
+        _onScreenFade.Raise(0f);
+        _playerSpawnedEvent.Raise();
     }
 }
