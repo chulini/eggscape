72efb32e79c7c05a0ef128f875595fca4791f89b
JQ <chulini@gmail.com>
2020-04-25 15:28:32 +0200

    audio sliders, mixer

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
 using UnityEngine;
+using UnityEngine.Audio;
 using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
@@ -12,22 +13,43 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource effectsAudioSource;
     [SerializeField] private AudioSource bgMusicAudioSource;
     [SerializeField] private AudioClip splatClip;
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] FloatReference fxVolumeVariable;
+    [SerializeField] FloatReference musicVolumeVariable;
 #pragma warning restore 0649
     private void OnEnable()
     {
         playerDied.AddListener(PlayerDiedHandler);
+        fxVolumeVariable.AddListener(FXVolumeChanged);
+        musicVolumeVariable.AddListener(MusicVolumeChanged);
+        Invoke("RefreshVolumes", 1f/30f);
     }
 
+    void RefreshVolumes()
+    {
+        FXVolumeChanged();
+        MusicVolumeChanged();
+    }
     private void OnDisable()
     {
         playerDied.RemoveListener(PlayerDiedHandler);
+        fxVolumeVariable.RemoveListener(FXVolumeChanged);
+        musicVolumeVariable.RemoveListener(MusicVolumeChanged);
     }
-
+    void FXVolumeChanged()
+    {
+        Debug.Log($"FXVol changed {fxVolumeVariable.Value}");
+        mixer.SetFloat("Effects", Mathf.Lerp(-80f, 0, Mathf.Pow(fxVolumeVariable.Value, .3f)));
+    }
+    void MusicVolumeChanged()
+    {
+        Debug.Log($"Music changed {musicVolumeVariable.Value}");
+        mixer.SetFloat("Music", Mathf.Lerp(-80f, 0, Mathf.Pow(musicVolumeVariable.Value, .3f)));
+    }
     private void Update()
     {
         bgMusicAudioSource.pitch = Time.timeScale;
     }
-
     private void PlayerDiedHandler()
     {
         effectsAudioSource.PlayOneShot(splatClip);   
